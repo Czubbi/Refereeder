c53f5d874356b55b7ce8841fb25a019b2ba030f6
Piotr Gzubicki <piotr.gzubicki97@gmail.com>
2019-04-11 16:20:40 +0200

    footer

--- Client/refereed-client/src/components/home/Home.js
+++ Client/refereed-client/src/components/home/Home.js
@@ -5,6 +5,8 @@ import Header from '../Header/Header.js';
 import Section from '../section/Section.js';
 import LoginModal from '../modals/LoginModal';
 import FederationCard from '../cards/FederationCard'
+import Footer from '../footer/Footer'
+import FooterNavigation from '../cards/FooterNavigation';
 
 class Home extends Component {
   constructor()
@@ -27,20 +29,65 @@ class Home extends Component {
               <div className="card-container">
                 <FederationCard>
                   <div className="card-top"></div>
-                  <div className="card-bottom"></div>
+                  <div className="card-bottom">
+                  <h4>Federation Name</h4>
+                  <p>Some text</p>
+                  </div>
                 </FederationCard>
                 <FederationCard>
                   <div className="card-top"></div>
-                  <div className="card-bottom"></div>
+                  <div className="card-bottom">
+                  <h4>Federation Name</h4>
+                  <p>Some text</p>
+                  </div>
                 </FederationCard>
                 <FederationCard>
                   <div className="card-top"></div>
-                  <div className="card-bottom"></div>
+                  <div className="card-bottom">
+                  <h4>Federation Name</h4>
+                  <p>Some text</p>
+                  </div>
                 </FederationCard>
               </div>
             </div>
           </div>
         </Section>
+        <Footer>
+          <div className='footer-wrapper'>
+            <div className='footer-content-left'>
+              <a href="/"><img src={process.env.PUBLIC_URL+'images/logo.png'}></img></a>
+            </div>
+            <div className='footer-content-right'>
+              <div className='footer-card-container'>
+                <FooterNavigation>
+                  <h3>Navigation:</h3>
+                    <ul>
+                      <li><a href='#'>Something</a></li>
+                      <li><a href='#'>Something</a></li>
+                      <li><a href='#'>Something</a></li>
+                      <li><a href='#'>Something</a></li>
+                      <li><a href='#'>Something</a></li>
+                    </ul>
+                </FooterNavigation>
+                <FooterNavigation>
+                  <h3>About:</h3>
+                    <ul>
+                      <li><a href='#'>Something</a></li>
+                      <li><a href='#'>Something</a></li>
+                      <li><a href='#'>Something</a></li>
+                    </ul>
+                </FooterNavigation>
+                <FooterNavigation>
+                  <h3>Help:</h3>
+                    <ul>
+                      <li><a href='#'>Something</a></li>
+                      <li><a href='#'>Something</a></li>
+                    </ul>
+                </FooterNavigation>
+              </div>
+            </div>
+          </div>
+        </Footer>
         <LoginModal modalVisible={this.state.modalVisible?'flex':'none'} modalPos={this.state.modalVisible?'0px':'-2000px'} onModalCloseClick={(e)=>{e.preventDefault();if(e.target==e.currentTarget){this.setState({modalVisible:false})}}}>
           <img style={{width:"70%",marginBottom:20,filter:'invert(100%)'}} src={process.env.PUBLIC_URL + "images/logo.png"} />
           <div>
