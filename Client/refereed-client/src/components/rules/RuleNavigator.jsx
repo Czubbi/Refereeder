9227de2a2319145bd09b6d3f4477a6cb1babe9dd
David <tropson90@gmail.com>
2019-05-20 14:46:09 +0200

    Arrows working on rules picker

--- Client/refereed-client/src/components/rules/RuleNavigator.jsx
+++ Client/refereed-client/src/components/rules/RuleNavigator.jsx
@@ -8,22 +8,23 @@ class RuleNavigator extends Component
         super(props);
         this.state={
             rules:props.rules,
-            hidden:true,
+            hidden:{},
         }
     }
 
-    changeHidden(){
-        if(this.state.hidden){
-            this.setState({
-                hidden:false
-            })
-        }else{
-            this.setState({
-                hidden:true
-            })
+    changeHidden(id){
+        var hidden=this.state.hidden;
+        if(hidden[`${id}`])
+        {
+            hidden[`${id}`]=false;
         }
+        else
+        {
+            hidden[`${id}`]=true;
+        }
+        console.log(hidden);
+        this.setState({hidden:hidden});
     }
-
     render()
     {
         return(
@@ -31,13 +32,13 @@ class RuleNavigator extends Component
                 {this.state.rules.map(rule=>{
                     return(
                         <div>
-                            <div className="rule-button-container" data-toggle="collapse" data-target={`#collapse_${rule._id}`} onClick={()=>{this.changeHidden()}}>
+                            <div className="rule-button-container" data-toggle="collapse" data-target={`#collapse_${rule._id}`} onClick={()=>{this.changeHidden(rule._id)}}>
                             <div onClick={()=>{this.props.onBtnClick({rule:rule,type:'rule'})}}  className="rule-button">
                                 <a href='#' title={rule.lang.eng.name} data-toggle="tooltip">{rule.number}. {rule.lang.eng.name.length>25?rule.lang.eng.name.substring(0,25)+'...':rule.lang.eng.name}</a>
                             </div>
                                 {rule.lang.eng.subRules.length>0?<div  className="rule-collapse-button" >{/*data-toggle="collapse" data-target={`#collapse_${rule._id}`}>*/}
                                     {/*$((`#collapse_${rule._id}`)).is(':hidden')?<i className="fas fa-arrow-down"></i>:<i className="fas fa-arrow-down open"></i>*/}
-                                    {this.state.hidden?<i className="fas fa-arrow-down"></i>:<i className="fas fa-arrow-down open"></i>}
+                                    {!this.state.hidden[`${rule._id}`]?<i className="fas fa-arrow-down"></i>:<i className="fas fa-arrow-down open"></i>}
                                 </div>:null}
                             </div>
                             {rule.lang.eng.subRules.length>0?<div className="collapse" id={`collapse_${rule._id}`}>
